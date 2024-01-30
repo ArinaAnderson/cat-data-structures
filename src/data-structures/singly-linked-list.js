8412d3f45406afc8ea6df3c8fe83ffa459b7db40
Marina Zinchenko <iolsoofuolso@gmail.com>
2024-01-29 23:37:52 -0800

    add methods getNode, pop, shift, undhift for SinglyLinkedList class

--- src/data-structures/singly-linked-list.js
+++ src/data-structures/singly-linked-list.js
@@ -5,6 +5,22 @@ class SinglyLinkedList {
     this.length = 0;
   }
 
+  getNode(idx) {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const iter = (count, node) => {
+      if (count === idx) {
+        return node;
+      }
+
+      return iter(count + 1, node.getNext());
+    };
+
+    return iter(0, this.head);
+  }
+
   push(node) {
     if (this.length === 0) {
       this.head = node;
@@ -14,8 +30,58 @@ class SinglyLinkedList {
       return this.length;
     }
 
-    this.tail.next = node;
+    this.tail.setNext(node);
     this.tail = node;
+    this.length += 1;
+
+    return this.length;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const newTail = this.getNode(this.length - 2);
+    const result = this.tail;
+    this.tail = newTail;
+    this.length -= 1;
+
+    return result;
+  }
+
+  shift() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const result = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+
+      return result;
+    }
+
+    const newHead = result.getNext();
+    this.head = newHead;
+    this.length -= 1;
+
+    return result;
+  }
+
+  unshift(node) {
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+      this.length += 1;
+      return this.length;
+    }
+
+    const currentHead = this.head;
+    node.setNext(currentHead);
+    this.head = node;
     this.length += 1;
 
     return this.length;
